4db91e49759d4eba0ff3297a5fc1b3d8937f2ddf
Neeraj2303 <neerajsukumaran2303@gmail.com>
2026-03-20 23:43:45 +0530

    fix(cursor): use react portal to escape framer-motion transforms in brutalist theme

--- src/themes/Brutalist/BrutalistTheme.jsx
+++ src/themes/Brutalist/BrutalistTheme.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import { portfolioData } from '../../data/portfolioData';
 import { motion } from 'framer-motion';
 import './BrutalistTheme.css';
